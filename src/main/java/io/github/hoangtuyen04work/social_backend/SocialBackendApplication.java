ed7f7a6708dad6494920bb8e6bbbc99824932420
hoangtuyen04work <164497260+hoangtuyen04work@users.noreply.github.com>
2025-03-12 11:21:32 +0700

    init project

---
+++ src/main/java/io/github/hoangtuyen04work/social_backend/SocialBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.hoangtuyen04work.social_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/hoangtuyen04work/social_backend/SocialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hoangtuyen04work.social_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
